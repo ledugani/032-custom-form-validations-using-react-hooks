834cccc5d28dc2cc82f767c0b58d954ba98545ce
Tom Dugan <thomas@aadwebb.com>
2020-04-09 11:53:41 -0500

    example use case of multiple variables with useEffect

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -2,16 +2,31 @@ import React, { useState, useEffect } from "react";
 
 export default function App({}) {
   const [email, setEmail] = useState("");
+  const [fullName, setFullName] = useState("");
 
   useEffect(() => {
     console.dir(email)
-  }, [email])
+  }, [email, fullName])
 
-  return <div>
-    <input
-      type="text"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-    />
-  </div>
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Your email"
+        />
+      </div>
+
+      <div>
+        <input
+          type="text"
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+          placeholder="Full name"
+        />
+      </div>
+    </div>
+  )
 }
